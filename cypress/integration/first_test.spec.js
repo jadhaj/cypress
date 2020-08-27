3b1a01a8b6994421f815968d8c38ffda8ded1dcd
Jad Haj Yahya <jhajyahy@redhat.com>
2020-08-27 15:01:15 +0300

    modified test

--- cypress/integration/first_test.spec.js
+++ cypress/integration/first_test.spec.js
@@ -1,9 +1,10 @@
+alert(Cypress.env('CACHE_FOLDER'));
 describe('Heading text Jad first test', () => {
     it('contains the correct title', () => {
         cy.visit('http://192.168.1.102:3000/example-1');
 
         cy.get('h1')
             .invoke('text')
-            .should('equal', 'My Awesome Web Application');
+            .should('equal', 'Awesome Web Application');
     });
 });
